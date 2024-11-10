02d54405b45741c15ce65c936b0dce6e5e1b453b
saihemanthnk <138007037+saihemanthnk@users.noreply.github.com>
2024-11-10 14:48:00 +0530

    Update route.js

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,6 +1,5 @@
 import {NextResponse} from 'next/server'
 import Resend from 'resend'
-import { EmailTemplate } from '../../../components/EmailTemplate';
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
